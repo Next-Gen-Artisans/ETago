7a3c3a30362700a0ec81056d21137b20d36287e4
ahleksu <roblesjohnalex18@gmail.com>
2023-12-04 15:27:51 +0800

    EDIT PROFILE EMAIL LOGIC DONE

--- app/src/main/java/com/nextgenartisans/etago/EditEmailActivity.java
+++ app/src/main/java/com/nextgenartisans/etago/EditEmailActivity.java
@@ -14,9 +14,15 @@ import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EditEmailActivity extends AppCompatActivity {
 
@@ -112,6 +118,73 @@ public class EditEmailActivity extends AppCompatActivity {
                 Intent i = new Intent(EditEmailActivity.this, ProfileActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        updateEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateEmailBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String currentPassword = editEmailPassInput.getText().toString();
+                        String newEmail = editEmailNewInput.getText().toString();
+                        String confirmEmail = editEmailConfirmInput.getText().toString();
+
+                        CustomSignInDialog customSignInDialog = new CustomSignInDialog(EditEmailActivity.this);
+
+                        if (currentPassword.isEmpty() || newEmail.isEmpty() || confirmEmail.isEmpty()) {
+                            Toast.makeText(EditEmailActivity.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        if (!newEmail.equals(confirmEmail)) {
+                            Toast.makeText(EditEmailActivity.this, "Emails do not match.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        customSignInDialog.setMessage("Updating Email...");
+                        customSignInDialog.showAuthProgress(true);
+                        customSignInDialog.show();
+
+                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        if (user != null) {
+                            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPassword);
+                            user.reauthenticate(credential).addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    user.verifyBeforeUpdateEmail(newEmail).addOnCompleteListener(task1 -> {
+                                        if (task1.isSuccessful()) {
+                                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                            db.collection("Users").document(user.getUid())
+                                                    .update("email", newEmail)
+                                                    .addOnSuccessListener(aVoid -> {
+                                                        customSignInDialog.setMessage("Verification email sent. Check your new email to confirm the change.");
+                                                        customSignInDialog.showAuthProgress(false);
+                                                    })
+                                                    .addOnFailureListener(e -> {
+                                                        customSignInDialog.setMessage("Failed to update email in Firestore.");
+                                                        customSignInDialog.showAuthFailedProgress(true);
+                                                    });
+                                        } else {
+                                            customSignInDialog.setMessage("Failed to send verification email.");
+                                            customSignInDialog.showAuthFailedProgress(true);
+                                        }
+                                        customSignInDialog.setProceedButtonVisible(true);
+                                        customSignInDialog.setProceedButtonClickListener(v -> customSignInDialog.dismiss());
+                                    });
+                                } else {
+                                    customSignInDialog.setMessage("Authentication failed.");
+                                    customSignInDialog.showAuthFailedProgress(true);
+                                }
+                                customSignInDialog.setProceedButtonVisible(true);
+                                customSignInDialog.setProceedButtonClickListener(v -> {
+                                    customSignInDialog.dismiss();
+                                    finish();
+                                });
+                            });
+                        }
+                    }
+                });
             }
         });
 
