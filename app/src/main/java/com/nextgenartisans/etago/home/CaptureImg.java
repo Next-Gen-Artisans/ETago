426b90d03e6a5f2318b9ffbab1efc2523050a5e9
ahleksu <roblesjohnalex18@gmail.com>
2023-12-09 15:46:17 +0800

    PREVIEW CAPTURE IMAGE FEATURE

--- app/src/main/java/com/nextgenartisans/etago/home/CaptureImg.java
+++ app/src/main/java/com/nextgenartisans/etago/home/CaptureImg.java
@@ -1,10 +1,19 @@
 package com.nextgenartisans.etago.home;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.view.PreviewView;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -14,9 +23,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,8 +35,15 @@ import android.widget.Toast;
 
 import com.nextgenartisans.etago.R;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 public class CaptureImg extends AppCompatActivity {
 
@@ -36,6 +54,8 @@ public class CaptureImg extends AppCompatActivity {
     private TextView headerTxt;
     private AppCompatImageView capturedImg;
     private AppCompatButton scanBtn, cancelBtn;
+    private FrameLayout frameLayout;
+    private PreviewView previewView;
 
     private ActivityResultLauncher<String[]> activityResultLauncher;
 
@@ -48,6 +68,10 @@ public class CaptureImg extends AppCompatActivity {
     private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
 
     private static final int CAMERA_REQUEST_CODE = 10;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
+
 
 
     @Override
@@ -69,14 +93,29 @@ public class CaptureImg extends AppCompatActivity {
         headerContainer = findViewById(R.id.header_container);
         headerTxt = findViewById(R.id.header_txt);
 
-        //Uploaded Image View
-        capturedImg = findViewById(R.id.captured_img);
+        //Captured Image View
+        //capturedImg = findViewById(R.id.captured_img);
+        frameLayout = findViewById(R.id.container);
+        previewView = findViewById(R.id.preview_view);
 
         //Buttons
-        scanBtn = findViewById(R.id.scan_btn);
+        scanBtn = findViewById(R.id.capture_scan_btn);
         cancelBtn = findViewById(R.id.cancel_btn);
         backBtn = findViewById(R.id.back_btn);
         saveBtn = findViewById(R.id.save_btn);
+
+        //View
+
+        imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,7 +153,51 @@ public class CaptureImg extends AppCompatActivity {
                 }
         );
 
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
 
+
+    }
+
+    private void takePicture() {
+        File file = new File(getExternalFilesDir(null), "photo.jpg");
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(file).build();
+
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(CaptureImg.this, "Image saved", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(CaptureImg.this, "Image unsaved" + exception.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageCapture);
     }
 
     private boolean hasCameraPermission() {
